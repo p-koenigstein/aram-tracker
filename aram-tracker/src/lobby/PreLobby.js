093f0dd64005f650ac04693b6dbb42b57c5b7f0c
Peter Königstein <pkonigstein@gmail.com>
2024-09-29 03:05:06 +0200

    lobbies looking better

--- aram-tracker/src/lobby/PreLobby.js
+++ aram-tracker/src/lobby/PreLobby.js
@@ -6,23 +6,30 @@ export function PreLobby({createLobby, joinLobby}) {
     const [lobbyId, setLobbyId] = useState("");
     return (
         <div>
-            <form
-                onSubmit={(e) => {
-                e.preventDefault()
-                joinLobby(lobbyId)
-            }}
+            <div className={"horiz"}>
+                <div className={"horiz defaultPadding"}>
+                <h3>Lobbies</h3>
+                </div>
+                <div className={"horiz defaultPadding"}>
+                    <Button onClick={createLobby} variant={"success"}>Create Lobby</Button>
+                </div>
+                <div className={"defaultPadding"}>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault()
+                        joinLobby(lobbyId)
+                    }}
                 >
-                <div className={"horiz"}>
-                    <Button onClick={createLobby}>Create LobbyPage</Button>
                     <input
                         type="text"
                         value={lobbyId}
-                        placeholder="LobbyPage ID"
+                        placeholder="Lobby ID"
                         onChange={(e) => setLobbyId(e.target.value)}
-                    />
-                    <Button type="submit" disabled={lobbyId===""}>Join LobbyPage</Button>
+                    /> &nbsp;
+                    <Button type="submit" disabled={lobbyId === ""} variant={"outline-success"}>Join Lobby</Button>
+                </form>
                 </div>
-        </form>
+            </div>
 </div>
 )
 }
